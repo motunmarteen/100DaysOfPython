0a978b536953235ea36e890a535a7272f478890f
motun <motunmarteen@gmail.com>
2022-12-08 17:35:38 +0100

    Write a program that interprets the Body Mass Index (BMI) based on a user's weight and height.

--- Day_0x02/bmi_2.0.py
+++ Day_0x02/bmi_2.0.py
@@ -8,15 +8,13 @@ weight = float(input("enter your weight in kg: "))
 #Write your code below this line 👇
 bmi = round(weight / height ** 2)
 
-if bmi <= 18:
-    print("Your BMI is " + str(bmi) + ", " + "you are underweight.")
-elif bmi <= 22:
-    print("Your BMI is " + str(bmi) + ", " + "you have a normal weight.")
-elif bmi <= 28:
-    print("Your BMI is " + str(bmi) + ", " + "you are slightly overweight.")
-elif bmi <= 33:
-    print("Your BMI is " + str(bmi) + ", " + "you are obese.")
-elif bmi >= 35:
-    print("Your BMI is " + str(bmi) + ", " + "you are clinically obese.")
-else:
-    print("Nothing")
+if bmi < 18.5:
+	print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+	print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+	print("Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+	print(f"Your BMI is {bmi}, you are obese.")
+elif:
+	print(f"Your BMI is {bmi} you are clinically obese.")
