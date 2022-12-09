eb13bab3a291e60b352a5e52cdeef5500b2946cc
motun <motunmarteen@gmail.com>
2022-12-09 11:59:53 +0100

    Multiple if statements in succession

--- Day_0x02/multiple_if_statement_in_succession.py
+++ Day_0x02/multiple_if_statement_in_succession.py
@@ -15,6 +15,9 @@ if height >= 120:
 	elif age <= 18:
 		bill = 7
 		print("Youth tickets are $7.")
+	
+	elif age >= 45 and age <= 55:
+		print("Everything is going to be ok. Have a free ride on us!")
 
 	else:
 		bill = 12
