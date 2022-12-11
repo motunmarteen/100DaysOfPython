1595c4f5ae4f6dc9e46e5c173d70a3c528bdbf55
motun <motunmarteen@gmail.com>
2022-12-11 22:10:49 +0100

    Randomisation in module

---
+++ Day_0x03/my_module.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env/python3
+
+pi = 3.14159246
---
+++ Day_0x03/random_module.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env/python3
+
+import random
+
+random_integer = random.randint(0, 5)
+#print(random_integer)
+
+random_float = random.random()
+#print(random_float)
+
+float_random = random_integer * random_float
+print(float_random)
