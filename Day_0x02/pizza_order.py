1043b3d9e6b7d423c65c95d0b3c0b0c756c52dc8
motun <motunmarteen@gmail.com>
2022-12-11 06:06:50 +0100

    Congratulations, you've got a job at Python Pizza. Your first job is to build an automatic pizza order program.

--- Day_0x02/pizza_order.py
+++ Day_0x02/pizza_order.py
@@ -8,23 +8,37 @@ extra_cheese = input("Do you want extra cheese? Y or N ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-
 bill = 0
 
 if size == "S":
-	bill += 15
-elif size == "M":
-	bill += 20
-else:
-	bill += 25
-
-if add_pepperoni == "Y":
-	if size == "S":
+	bill = 15
+	
+	if add_pepperoni == "Y":
 		bill += 2
-	else:
+	
+	if extra_cheese == "Y":
+		bill += 1
+	
+	print(f"Your final bill is: ${bill}")
+
+elif size == "M":
+	bill = 20
+
+	if add_pepperoni == "Y":
 		bill += 3
 
-if extra_cheese == "Y":
-	bill += 1
+	if extra_cheese == "Y":
+		bill += 1
 
-print(f"Your final bill is ${bill}")
+	print(f"Your final bill is: ${bill}")
+
+else:
+	bill = 25
+	
+	if add_pepperoni == "Y":
+		bill += 3
+
+	if extra_cheese == "Y":
+		bill += 1
+
+	print(f"Your final bill is: ${bill}")
