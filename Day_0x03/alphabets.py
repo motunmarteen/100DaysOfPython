67889c0df86c667f7f6111aa7b5ce49da70ec973
motun <motunmarteen@gmail.com>
2022-12-16 11:24:53 +0100

    write a program that will mark a spot with an X.

---
+++ Day_0x03/alphabets.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env/python3
+
+vowels = ["a", "e", "i", "o", "u"]
+consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+vowel_len = int(len(vowels))
+consonants_len = int(len(consonants))
+alphabets = [vowels, consonants]
+print(f"{alphabets}")
+print(vowel_len + consonants_len)
---
+++ Day_0x03/choice.py
@@ -0,0 +1,5 @@
+import random
+
+name = input("names here").split()
+names = random.choice(name)
+print(f"{names}")
