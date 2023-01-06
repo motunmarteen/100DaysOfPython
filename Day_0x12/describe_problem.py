7030157fe2b2def79613a33b872a7a27ae75c244
motun <motunmarteen@gmail.com>
2023-01-06 17:03:40 +0100

    Describing problems

---
+++ Day_0x12/describe_problem.py
@@ -0,0 +1,15 @@
+# Describe Problem
+
+# def my_function():
+#   for i in range(1, 20):
+#     if i == 20:
+#       print("You got it")
+# my_function()
+
+#The code above will not print "You got it" because 20 is a stop in the Range
+
+def my_function():
+  for i in range(1, 21):
+    if i == 20:
+      print("You got it")
+my_function()
