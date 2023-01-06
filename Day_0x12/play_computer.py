1aca824d593f457b41c75ff93ae577823e369759
motun <motunmarteen@gmail.com>
2023-01-06 17:17:53 +0100

    play Computer

---
+++ Day_0x12/play_computer.py
@@ -0,0 +1,16 @@
+# Play Computer
+
+#year = int(input("What's your year of birth?"))
+#if year > 1980 and year < 1994:
+#  print("You are a millenial.")
+#elif year > 1994:
+#  print("You are a Gen Z.")
+
+#This code above shows that the year 1994 is not classified in any of the if statements. for it to be classified, there has to be an equate of 1994, to solve this
+
+# Play Computer
+year = int(input("What's your year of birth?: "))
+if year > 1980 and year < 1994:
+  print("You are a millenial.")
+elif year >= 1994:
+  print("You are a Gen Z.")
